818a17687cf4f884daf35ed9baafd09445b501f8
hasanaydogar <hasanaydogar@gmail.com>
2015-03-30 15:48:06 +0300

    First Commit

---
+++ ASPPatterns.Chap2.Service/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap2.Service
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IList<Product> GetAllProductsIn(int categoryId)
+        {
+            IList<Product> products = new List<Product>();
+
+            //Database Operation to populate products
+
+            return products;
+
+        }
+    }
+}
