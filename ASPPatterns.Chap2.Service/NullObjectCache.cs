9728e34f2767713c525f2744778d34c9285407fa
hasanaydogar <hasanaydogar@gmail.com>
2015-03-30 16:29:03 +0300

    commit

---
+++ ASPPatterns.Chap2.Service/NullObjectCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap2.Service
+{
+    public class NullObjectCache : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+            // Do Nothing
+        }
+
+        public void Store(string key, object data)
+        {
+          // Do Nothing
+        }
+
+        public T Retrieve<T>(string key)
+        {
+            return default(T);
+        }
+    }
+}
