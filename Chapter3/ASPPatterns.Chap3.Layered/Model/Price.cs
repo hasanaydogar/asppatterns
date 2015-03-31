1737c3a7d0f491050b12253a210de742534c6b97
hasanaydogar <hasanaydogar@gmail.com>
2015-03-31 16:33:45 +0300

    business logic layer

---
+++ Chapter3/ASPPatterns.Chap3.Layered/Model/Price.cs
@@ -0,0 +1,42 @@
+﻿namespace Model
+{
+    public class Price
+    {
+        private IDiscountStrategy _discountStrategy = new NullDiscountStrategy();
+        private readonly decimal _rrp;
+        private readonly decimal _sellingPrice;
+
+        public Price(decimal rrp, decimal sellingPrice)
+        {
+            _rrp = rrp;
+            _sellingPrice = sellingPrice;
+        }
+
+        public void SetDiscountStrategyTo(IDiscountStrategy discountStrategy)
+        {
+            _discountStrategy = discountStrategy;
+
+        }
+
+        public decimal SellingPrice {
+            get { return _discountStrategy.ApplyExtraDiscountsTo(_sellingPrice); }
+        }
+
+        public decimal Rrp
+        {
+            get { return _rrp; }
+        }
+
+        public decimal Discount
+        {
+            get
+            {
+                if (Rrp>SellingPrice)
+                {
+                    return 1 - (SellingPrice/Rrp);
+                }
+                return 0;
+            }
+        }
+    }
+}
---
+++ Chapter3/ASPPatterns.Chap3.Layered/Model/TradeDiscountStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class TradeDiscountStrategy : IDiscountStrategy
+    {
+        public decimal ApplyExtraDiscountsTo(decimal originalSalesPrice)
+        {
+            decimal price = originalSalesPrice;
+
+            price = price*0.95M;
+
+            return price;
+        }
+    }
+}
