f7a04c43ff65c8edc91c3b3e7a17b6626acf75f6
hasanaydogar <hasanaydogar@gmail.com>
2015-04-07 08:10:13 +0300

    c4

---
+++ Chapter4/ASPPatterns.C4.ActiveRecord/Model/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Castle.ActiveRecord;
+
+namespace Model
+{
+   [ActiveRecord("Comments")]
+   public class Comment : ActiveRecordBase<Comment>
+   {
+       [PrimaryKey]
+       public int Id { get; set; }
+
+       [Property]
+       public string Text { get; set; }
+
+       [Property]
+       public string Author { get; set; }
+
+       [Property]
+       public DateTime DateAdded { get; set; }
+
+       [BelongsTo("PostID")]
+       public Post Post { get; set; }
+   }
+}
---
+++ Chapter4/ASPPatterns.C4.ActiveRecord/Model/Post.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.ActiveRecord;
+using Castle.ActiveRecord.Queries;
+
+namespace Model
+{
+    public class Post : ActiveRecordBase<Post>
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [Property]
+        public string Subject { get; set; }
+
+        [Property]
+        public string Text { get; set; }
+
+        public string ShortText
+        {
+            get {
+                if (Text.Length > 20)
+                {
+                    return Text.Substring(0, 20) + "...";
+                }
+                return Text;
+            }
+        }
+        [HasMany]
+        public IList<Comment> Comments { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public static Post FindLatestPost()
+        {
+            SimpleQuery<Post> q = new SimpleQuery<Post>(@"from Post p order by p.DateAdded desc");
+
+            return (Post) q.Execute()[0];
+        }
+    }
+}
